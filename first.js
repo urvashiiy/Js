299f24574f777fdc75b64320ed0c3d495225c43f
Urvashi Pandey <urvaship748@gmai.com>
2024-08-11 22:06:35 +0530

    k

---
+++ first.js
@@ -0,0 +1,11 @@
+console.log('Hello My first');
+
+FullName= 'Urvashi Pandey'
+console.log(FullName); 
+console.log("FullName");  //If you write  in "" it will print as it is
+
+x = null; //pata hai ki null mein koi kuch value hai
+y = undefined; //pata hi nhi hai
+
+
+
