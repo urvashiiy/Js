18e6fb376fd0ce39fe6a7f1dc3bb119d106e125e
Urvashi Pandey <urvaship748@gmai.com>
2024-08-12 22:57:36 +0530

    .,

--- baisc files/operator.js
+++ baisc files/operator.js
@@ -1,4 +1,54 @@
 // operators questions
-let a = 10;
+// let a = 10;
+// let b = 5;
+// console.log("a =",a,"b=",b);
+//console.log("a + b =",a+b);
+//console.log("a - b =",a-b);
+//console.log("a * b =",a*b);
+//console.log("a / b =",a/b);
+//console.log("a ** b =",a**b); // 10^5
+
+
+//unary operators
+//a++; //11 
+//b--; //4
+//console.log("a =",a);
+// console.log("b =",b);
+// console.log("++a =",++a); //11
+
+// console.log("a++ =",a++); //10 this is how works. same for the --
+// console.log("a =",a); //11
+
+//Assignment operator
+// let a = 8;
+// let b = 5;
+// console.log("a =",a,"b=",b);
+// a **= 2; //add 
+// console.log("a =",a);
+
+//comparison operator
+// let a = 8;
+// let b = 5;
+// console.log("a =", a, "b=", b);
+// console.log("a === b =", a === b); //true or false 
+// console.log("a !== b =", a !== b); //true or false
+// console.log("a > b =", a > b); //true or false
+// console.log("a < b =", a < b); //true or false and many more 
+
+
+// now we learn about logical operator
+
+let a = 8;
 let b = 5;
-console.log("a + b =",a+b)
+
+let cond1 = a > b;
+let cond2 = a === 100;
+console.log("cond1 && cond2 =", cond1 && cond2); //false
+console.log("cond1 || cond2 =", cond1 || cond2); //true
+console.log("!(8<5) =", !(a<b)); //true
+
+
+
+
+
+
