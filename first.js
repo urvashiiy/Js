d08af4abe9ff5444d2d91c228db9551f01461008
Urvashi Pandey <urvaship748@gmai.com>
2024-08-12 15:13:12 +0530

    day2

--- first.js
+++ first.js
@@ -1,11 +1,21 @@
 console.log('Hello My first');
 
-FullName= 'Urvashi Pandey'
-console.log(FullName); 
+FullName = 'Urvashi Pandey'
+console.log(FullName);
 console.log("FullName");  //If you write  in "" it will print as it is
 
 x = null; //pata hai ki null mein koi kuch value hai
 y = undefined; //pata hi nhi hai
 
 
+const student = {
+    fullName:"Urvashi",
+    age: 22,
+    address: "Lucknow",
+    hobbies: ["Dancing", "Singing", "Playing"],
+    cgpa: 8.5
+
+};
+console.log(student["age"]);  // these are two ways to access the keys
+console.log(student.address);
 
